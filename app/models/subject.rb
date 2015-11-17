03ae094a2490f52c570c3cab2a187d677ef2567a
Josef Zacek <info@josefzacek.com>
2015-11-17 23:55:33 +0000

    Revert "defined custom scopes"
    This reverts commit 48de52b8075ab88f509853397871250872b74a0f.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -11,23 +11,4 @@
 #
 
 class Subject < ActiveRecord::Base
-  scope :visible, lambda {
-    where(visible: true)
-  }
-
-  scope :invisible, lambda {
-    where(visible: false)
-  }
-
-  scope :sorted, lambda {
-    order('subjects.position ASC')
-  }
-
-  scope :newest_first, lambda {
-    order('subjects.created_at DESC')
-  }
-
-  scope :search, lambda {|search|
-    where('name LIKE ?', "%#{search}%")
-  }
 end
