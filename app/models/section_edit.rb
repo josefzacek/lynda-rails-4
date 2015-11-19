7b5c0670497a0d6f604ae1628906e96f13eadb6e
Josef Zacek <info@josefzacek.com>
2015-11-19 23:08:17 +0000

    running annotate to get schema information

--- app/models/section_edit.rb
+++ app/models/section_edit.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: section_edits
+#
+#  id            :integer          not null, primary key
+#  admin_user_id :integer
+#  section_id    :integer
+#  summary       :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class SectionEdit < ActiveRecord::Base
   belongs_to :editor, class_name: 'AdminUser', foreign_key: 'admin_user_id'
   belongs_to :section
--- test/models/section_edit_test.rb
+++ test/models/section_edit_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: section_edits
+#
+#  id            :integer          not null, primary key
+#  admin_user_id :integer
+#  section_id    :integer
+#  summary       :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class SectionEditTest < ActiveSupport::TestCase
