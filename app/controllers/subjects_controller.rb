47000cefb70ce9fd71f9b3027e7d1537b5395812
Josef Zacek <info@josefzacek.com>
2015-11-22 22:07:07 +0000

    generated Demo controller with index action

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,6 @@
 class SubjectsController < ApplicationController
+  layout false
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'demo/index'
+
   root 'demo#index'
 
-  match ':controller(/action(/:id))', via: :get
+  match ':controller(/:action(/:id))', via: :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
