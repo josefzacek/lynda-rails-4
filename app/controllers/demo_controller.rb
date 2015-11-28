ff9b19205f25e219851f034706c67df0e8e64a5e
Josef Zacek <info@josefzacek.com>
2015-11-28 22:23:19 +0000

    moved text used for text_helpers method to demo model

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,7 +3,7 @@ class DemoController < ApplicationController
   end
 
   def text_helpers
-    @text = "This is some long sample text. It includes line breaks\n and double-line breaks.\n\n It rambles on and on and on. \n\n Until we get to last line."
+    @text = Demo.text
   end
 
   def number_helpers
--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,2 +1,5 @@
 class Demo < ActiveRecord::Base
+  def self.text
+    "This is some long sample text. It includes line breaks\n and double-line breaks.\n\n It rambles on and on and on. \n\n Until we get to last line."
+  end
 end
