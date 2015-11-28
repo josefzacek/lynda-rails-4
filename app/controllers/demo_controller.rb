67e107333d08450ffad585ce818232b84b8ff016
Josef Zacek <info@josefzacek.com>
2015-11-28 22:01:41 +0000

    demo controller - number_helpers method

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -5,4 +5,7 @@ class DemoController < ApplicationController
   def text_helpers
     @text = "This is some long sample text. It includes line breaks\n and double-line breaks.\n\n It rambles on and on and on. \n\n Until we get to last line."
   end
+
+  def number_helpers
+  end
 end
