170c6e993439da467af555749e4dcad0d33f960d
Josef Zacek <info@josefzacek.com>
2015-11-28 20:54:37 +0000

    demo - text_helper method

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,8 @@
 class DemoController < ApplicationController
   def index
   end
+
+  def text_helpers
+    @text = "This is some long sample text. It includes line breaks\n and double-line breaks.\n\n It rambles on and on and on. \n\n Until we get to last line."
+  end
 end
