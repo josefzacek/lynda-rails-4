365a4c02dcdf9bf446d57d2c052fd3c90e70a838
Josef Zacek <info@josefzacek.com>
2015-11-29 21:46:21 +0000

    status_tag helper for visible value

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def status_tag(boolean, options = {})
+    options[:true_text] ||= '<i class="fa fa-check"></i>'.html_safe
+    options[:false_text] ||= '<i class="fa fa-times"></i>'.html_safe
+
+    if boolean
+      content_tag(:span, options[:true_text],
+                  class: 'status true', style: 'color: green')
+    else
+      content_tag(:span, options[:false_text],
+                  class: 'status false', style: 'color: red')
+    end
+  end
 end
