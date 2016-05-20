7ca540488e33eefab7a5d8906032dc39e9aa6fef
Josef Zacek <info@josefzacek.com>
2016-05-21 00:44:48 +0100

    logout method

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -25,3 +25,11 @@
       redirect_to(action: 'login')
     end
   end
+
+  def logout
+    session[:user_id] = nil
+    session[:username] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to(action: 'login')
+  end
+end
