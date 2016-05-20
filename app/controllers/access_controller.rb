ea1c5f827a5f0835f7df8c9d39957df10fc1cc1d
Josef Zacek <info@josefzacek.com>
2016-05-21 00:44:36 +0100

    attempt_login method

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -7,3 +7,21 @@
   def login
   end
 
+  def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = AdminUser.where(username: params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+
+    if authorized_user
+      session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
+      flash[:notice] = 'You are now logged in!'
+      redirect_to(action: 'index')
+    else
+      flash[:notice] = 'Invalid username/password combination!'
+      redirect_to(action: 'login')
+    end
+  end
