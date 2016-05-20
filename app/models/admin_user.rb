78810d341ce0c12bba4e51b258a7d7fb55c76bb4
Josef Zacek <info@josefzacek.com>
2016-05-20 22:56:03 +0100

    validations for first_name, last_name, username

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -19,6 +19,15 @@ class AdminUser < ActiveRecord::Base
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
   FORBIDDEN_USERNAMES = %w(dublinireland has_manyelloworld superadmin)
+
+  validates_presence_of :first_name
+  validates_length_of :first_name, maximum: 25
+  validates_presence_of :last_name
+  validates_length_of :last_name, maximum: 50
+  validates_presence_of :username
+  validates_length_of :username, within: 8..25
+  validates_uniqueness_of :username
+
   # shortcut validation
   validates :email, presence: true,
                     length: { maximum: 50 },
