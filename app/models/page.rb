8cf5ab519b22daf3fef9896ea14b12764f5fa97f
Josef Zacek <info@josefzacek.com>
2016-05-20 22:50:34 +0100

    validate uniqueness of permalink

--- app/models/page.rb
+++ app/models/page.rb
@@ -19,6 +19,7 @@ class Page < ActiveRecord::Base
 
   validates_presence_of :name
   validates_length_of :name, maximum: 255
+  validates_uniqueness_of :pemalink
 
   scope :visible, lambda {
     where(visible: true)
